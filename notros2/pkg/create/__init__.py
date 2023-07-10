5aac5f92e122ad4db28cfe0160a574600cb5c862
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2023-07-10 21:45:05 +0900

    Working with sample nodes.

--- notros2/pkg/create/__init__.py
+++ notros2/pkg/create/__init__.py
@@ -5,7 +5,8 @@ import pathlib
 from .ament_cmake import \
     create_package_xml_from_template, \
     create_cmakelists_txt_from_template, \
-    create_cpp_for_nodes
+    create_cpp_for_nodes, \
+    create_hpp_for_nodes
 
 
 def create_package_template(path: pathlib.Path, config: dict, args: argparse.Namespace) -> None:
--- notros2/pkg/create/ament_cmake/__init__.py
+++ notros2/pkg/create/ament_cmake/__init__.py
@@ -1,3 +1,5 @@
 from ._package_xml_template import create_package_xml_from_template
 from ._package_cmakelists_txt_template import create_cmakelists_txt_from_template
-from ._add_node import create_cpp_for_nodes
+from ._add_node import \
+    create_cpp_for_nodes, \
+    create_hpp_for_nodes
